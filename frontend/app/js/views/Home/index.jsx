450848b7fc4d3c8c6b45817a75f0f1cb33045fc5
theflametrooper <theflametrooper@gmail.com>
2017-04-15 21:56:39 +0100

    Added a simple redux counter example

--- frontend/app/js/views/Home/index.jsx
+++ frontend/app/js/views/Home/index.jsx
@@ -1,10 +1,41 @@
-import React, { Component } from "react";
+import React, { Component, PropTypes } from "react";
+import { connect } from "react-redux";
+import { increment } from "actions/app";
+
+@connect(state => ({
+	counter: state.app.get("counter"),
+}))
 
 export default class Home extends Component {
+	static propTypes = {
+		counter: PropTypes.number,
+		dispatch: PropTypes.func,
+	}
+
+	constructor() {
+		super();
+
+		this.handleIncrement = this.handleIncrement.bind(this);
+	}
+
+	handleIncrement() {
+		const { dispatch } = this.props;
+
+		dispatch(increment());
+	}
+
 	render() {
+		const {
+			counter,
+		} = this.props;
+
 		return (
 			<div>
 				<h2>Welcome to Musare!</h2>
+				<h3>{ counter }</h3>
+				<button onClick={ this.handleIncrement }>
+					Increase counter
+				</button>
 			</div>
 		);
 	}
