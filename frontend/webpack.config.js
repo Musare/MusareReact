3ee77515e40282aa329506b3e89835fee3ad1baf
theflametrooper <theflametrooper@gmail.com>
2017-04-15 21:09:33 +0100

    Fixed a webpack depreciation warning

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -22,7 +22,7 @@ const plugins = [
 	}),
 	new webpack.optimize.OccurrenceOrderPlugin(),
 	new webpack.HotModuleReplacementPlugin(),
-	new webpack.NoErrorsPlugin(),
+	new webpack.NoEmitOnErrorsPlugin(),
   new webpack.DefinePlugin({
     "process.env": {
 			NODE_ENV: JSON.stringify(nodeEnv),
