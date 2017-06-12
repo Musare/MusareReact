18619e6a4008074b4fd12219da21e18f81788502
howdoyoucode <theflametrooper@gmail.com>
2017-06-12 23:03:49 +0100

    Auth related buttons should be variable on whether user is logged in

--- frontend/app/js/components/Global/Menu.jsx
+++ frontend/app/js/components/Global/Menu.jsx
@@ -1,19 +1,36 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import { IndexLink, Link } from "react-router";
 
+@connect(state => ({
+	loggedIn: state.user.get("loggedIn"),
+}))
+
 export default class Menu extends Component {
+	static propTypes = {
+		loggedIn: PropTypes.bool,
+	}
+
 	render() {
 		return (
 			<div className="Menu">
 				<IndexLink to="/">
 					Home
 				</IndexLink>
-				<Link to="login">
-					Login
-				</Link>
-				<Link to="register">
-					Register
-				</Link>
+				{ this.props.loggedIn ?
+					<Link to="logout">
+						Logout
+					</Link> :
+					<div>
+						<Link to="login">
+							Login
+						</Link>
+						<Link to="register">
+							Register
+						</Link>
+					</div>
+				}
 				<Link to="template">
 					Template
 				</Link>
